743d17be2cc97b0eb06432ee35d3dbf079edce4d
Iyan <iyanfef@gmail.com>
2022-10-18 10:18:49 +0200

    añadido nombre a concesionario

--- src/com/iyan/proyectoPrueba/Concesionario/Concesionario.java
+++ src/com/iyan/proyectoPrueba/Concesionario/Concesionario.java
@@ -2,4 +2,14 @@ package com.iyan.proyectoPrueba.Concesionario;
 
 public class Concesionario {
 
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
 }
