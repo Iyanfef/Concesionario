beed73d7c912e9902241693cb8017fea2c3a9b94
Iyan <iyanfef@gmail.com>
2022-10-21 12:08:46 +0200

    nueva clase

--- src/com/iyan/proyectoPrueba/Concesionario/Concesionario.java
+++ src/com/iyan/proyectoPrueba/Concesionario/Concesionario.java
@@ -2,16 +2,5 @@ package com.iyan.proyectoPrueba.Concesionario;
 
 public class Concesionario {
 
-	private String nombre;
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	
-	//prueba de github
-	// mas comentarios de prueb
+	//nueva clase
 }
